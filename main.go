2988c054facc8ba14640e740153ac4f77dc81821
Joshua Stauter <jstauter@jungroup.com>
2015-12-08 20:50:07 -0500

    css

--- main.go
+++ main.go
@@ -62,39 +62,37 @@ func getbody(url string) []byte {
 // grooveshark.im
 // groovesharks.org
 
-func client(w http.ResponseWriter, r *http.Request) {
-	ip := net.ParseIP(strings.Trim(r.RemoteAddr[0:strings.LastIndex(r.RemoteAddr, ":")], "[]"))
-
-	switch r.Method + " " + r.URL.EscapedPath() {
-	case "POST /playlist":
-		name := r.FormValue("name")
-		artist := r.FormValue("artist")
-		youtube := struct{ ID string }{}
-		err := json.Unmarshal(getbody("http://grooveshark.im/music/getYoutube?track="+url.QueryEscape(name)+"&artist="+url.QueryEscape(artist)), &youtube)
-		if err != nil {
-			panic(err)
-		}
-		song := Song{IP: ip, ID: youtube.ID, Time: time.Now(), Name: name, Artist: artist}
-		playlist = append(playlist, song)
-		votes = append(votes, Vote{IP: ip, ID: youtube.ID, Time: time.Now()})
-	case "GET /playlist":
-		body, err := json.Marshal(playlist)
-		if err != nil {
-			panic(err)
-		}
-		w.Write(body)
-	case "POST /votes":
-		votes = append(votes, Vote{IP: ip, ID: r.FormValue("id"), Time: time.Now()})
-	case "GET /search":
-		w.Write(getbody("http://grooveshark.im/music/typeahead?query=" + url.QueryEscape(r.URL.Query().Get("q"))))
-	default:
-		http.ServeFile(w, r, "client.html")
-	}
-}
-
 func main() {
 	go http.ListenAndServe("localhost:"+os.Getenv("PORT"), &player{})
 
-	http.HandleFunc("/", client)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ip := net.ParseIP(strings.Trim(r.RemoteAddr[0:strings.LastIndex(r.RemoteAddr, ":")], "[]"))
+
+		switch r.Method + " " + r.URL.EscapedPath() {
+		case "POST /playlist":
+			name := r.FormValue("name")
+			artist := r.FormValue("artist")
+			youtube := struct{ ID string }{}
+			err := json.Unmarshal(getbody("http://grooveshark.im/music/getYoutube?track="+url.QueryEscape(name)+"&artist="+url.QueryEscape(artist)), &youtube)
+			if err != nil {
+				panic(err)
+			}
+			song := Song{IP: ip, ID: youtube.ID, Time: time.Now(), Name: name, Artist: artist}
+			playlist = append(playlist, song)
+			votes = append(votes, Vote{IP: ip, ID: youtube.ID, Time: time.Now()})
+		case "GET /playlist":
+			body, err := json.Marshal(playlist)
+			if err != nil {
+				panic(err)
+			}
+			w.Write(body)
+		case "POST /votes":
+			votes = append(votes, Vote{IP: ip, ID: r.FormValue("id"), Time: time.Now()})
+		case "GET /search":
+			w.Write(getbody("http://grooveshark.im/music/typeahead?query=" + url.QueryEscape(r.URL.Query().Get("q"))))
+		default:
+			http.ServeFile(w, r, "client.html")
+		}
+	})
 	http.ListenAndServe(":80", nil)
 }
